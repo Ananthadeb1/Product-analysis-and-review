8914249e09860ac348bff5c7221e33bba13f5ca5
Anantha debnath <88941537+Ananthadeb1@users.noreply.github.com>
2022-04-05 20:54:16 +0600

    overall done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Chart from './components/chart/Chart';
+import Header from './components/header/Header';
+import Home from './components/Home/Home';
+import useReviews from './components/hooks/useReviews';
+import Review from './components/Review/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Anantha Debnath
+    <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/chart' element={<Chart></Chart>}></Route>
+      </Routes>
     </div>
   );
 }
